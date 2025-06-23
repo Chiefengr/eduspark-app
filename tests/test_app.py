922572044deff22109a8000057f66960fde45b4d
Chiefengr <temmykore01@gmail.com>
2025-06-23 07:14:25 -0700

    Update CI/CD to push Docker image to Docker Hub

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,24 @@
-from app.main_flask import app
+import sys
+import os
+import pytest
+
+# ✅ Dynamically add the parent directory to sys.path (where `app/` lives)
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..')))
+
+try:
+    from app.main_flask import app
+except ModuleNotFoundError as e:
+    raise RuntimeError()
 
 
-def test_home_route():
-    tester = app.test_client()
-    response = tester.get("/")
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+
+def test_home_route(client):
+    response = client.get('/')
     assert response.status_code == 200
     assert b"EduSpark" in response.data
